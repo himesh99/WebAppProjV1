f22a286f4399d37c2b757898a14d1c14049e876b
himesh99 <himesh.patel@hmcts.net>
2024-07-15 16:31:52 +0100

    infra

--- variables.tf
+++ variables.tf
@@ -2,3 +2,11 @@ variable "resource_group" {
   type = string
   default = "patelh-sandbox-rg"
 }
+variable "tfversion" {
+  type = string
+  default = "1.3.7"
+}
+variable "workingDirectory" {
+  type = string
+  default = "./"
+}
