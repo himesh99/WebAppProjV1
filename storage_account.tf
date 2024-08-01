fcd5c6ff7551420dc792f787b65e6c475493fddb
himesh99 <himesh.patel@hmcts.net>
2024-08-01 17:09:57 +0100

    Disallowing public access

--- storage_account.tf
+++ storage_account.tf
@@ -6,6 +6,8 @@ resource "azurerm_storage_account" "storageaccount" {
     location = var.location
     account_tier = "Standard"
     account_replication_type = "LRS"
+    public_network_access_enabled = false
+
 
   
 }
