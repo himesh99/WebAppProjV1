34de6aa8c5a55f837f349024af0a806cdbce0e3e
himesh99 <himesh.patel@hmcts.net>
2024-08-01 12:53:08 +0100

    name change

--- virtual_network.tf
+++ virtual_network.tf
@@ -1,7 +1,7 @@
 
 
 resource "azurerm_virtual_network" "vnet_test" {
-  resource_group_name = azurerm_resource_group.himesh_rg
+  resource_group_name = var.resource_group_hp
   name = "vnet_test"
   location = var.location
   address_space = ["10.0.0.0/16"]
