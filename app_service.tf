b51f581eaa47b05764b551f23e78297e0f565761
himesh99 <himesh.patel@hmcts.net>
2024-08-02 15:13:36 +0100

    app service and app plan

---
+++ app_service.tf
@@ -0,0 +1,6 @@
+resource "azurerm_app_service" "wordpresswebapp" {
+   name  = "wordpresswebapp"
+    resource_group_name = var.resource_group_hp
+    location = var.location
+    app_service_plan_id = azurerm_app_service_plan.wordpress_plan
+}
---
+++ app_service_plan.tf
@@ -0,0 +1,9 @@
+resource "azurerm_app_service_plan" "wordpress_plan" {
+    name  = "wordpressapp"
+    resource_group_name = var.resource_group_hp
+    location = var.location
+    sku {
+      tier = "Standard"
+      size = "S1"
+    }
+}
