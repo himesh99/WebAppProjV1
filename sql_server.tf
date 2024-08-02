ca0636db5b5d6c203d1d41d0faf028c82beb5afe
himesh99 <himesh.patel@hmcts.net>
2024-08-02 15:46:47 +0100

    sql server

---
+++ sql_server.tf
@@ -0,0 +1,8 @@
+resource "azurerm_sql_server" "hp_server" {
+    name  = "hpSQLServer"
+    resource_group_name = var.resource_group_hp
+    location = var.location
+    version = "12.0"
+    administrator_login          = "hpadministrator"
+    administrator_login_password = "thisLion99"
+}
