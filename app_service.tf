c89a920e1c1314cb88cb1853d261e77de8bf1701
himesh99 <himesh.patel@hmcts.net>
2024-08-02 15:26:38 +0100

    change v2

--- app_service.tf
+++ app_service.tf
@@ -1,10 +1,6 @@
-resource "azurerm_windows_web_app" "wordpresswebapp" {
+resource "azurerm_app_service" "wordpresswebapp" {
    name  = "wordpresswebapp"
     resource_group_name = var.resource_group_hp
     location = var.location
-    service_plan_id = azurerm_app_service_plan.wordpress_plan
-
-    site_config {
-      
-    }
+    app_service_plan_id = azurerm_app_service_plan.wordpress_plan.id
 }
--- app_service_plan.tf
+++ app_service_plan.tf
@@ -2,7 +2,7 @@ resource "azurerm_app_service_plan" "wordpress_plan" {
     name  = "wordpressapp"
     resource_group_name = var.resource_group_hp
     location = var.location
-    sku {
+    sku{
       tier = "Standard"
       size = "S1"
     }
