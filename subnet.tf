5d09db698e743d5bfcb8b8e0fd296297e4de0773
himesh99 <himesh.patel@hmcts.net>
2024-08-02 18:20:09 +0100

    add subnet

--- subnet.tf
+++ subnet.tf
@@ -2,6 +2,7 @@ resource "azurerm_subnet" "hp_subnet" {
     name = "subnethp"
     resource_group_name = var.resource_group_hp
     address_prefixes     = ["10.0.1.0/24"]
+    service_endpoints    = ["Microsoft.Storage"]
     virtual_network_name = azurerm_virtual_network.vnet_test.name
 
   
