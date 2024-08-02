b9245dc4a205f6ecefeffc89c68b55dc87d68bc6
himesh99 <himesh.patel@hmcts.net>
2024-08-02 17:59:57 +0100

    change

--- storage_account.tf
+++ storage_account.tf
@@ -16,5 +16,5 @@ resource "azurerm_storage_account" "storageaccount" {
 resource "azurerm_storage_container" "containerhp" {
     name  = "containerhp"
     storage_account_name = azurerm_storage_account.storageaccount.name
-    container_access_type = "private"
+
 }
