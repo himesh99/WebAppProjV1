0a8b22d256ebf7c4dae5dbc2d8565241de84c708
Himesh <himesh.patel99@hotmail.com>
2024-09-04 18:03:23 +0100

    sa

--- storage_account.tf
+++ storage_account.tf
@@ -1,26 +1,26 @@
 
 
-# resource "azurerm_storage_account" "storageaccount" {
-#     name  = "storageaccounthpwebapp"
-#     resource_group_name = var.resource_group_hp
-#     location = var.location
-#     account_tier = "Standard"
-#     account_replication_type = "LRS"
-#     public_network_access_enabled = true
-#     allow_nested_items_to_be_public = false
+resource "azurerm_storage_account" "storageaccount" {
+    name  = "storageaccounthpwebapp"
+    resource_group_name = var.resource_group_hp
+    location = var.location
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+    public_network_access_enabled = true
+    allow_nested_items_to_be_public = false
 
-#     network_rules {
-#     default_action             = "Allow"
-#     virtual_network_subnet_ids = [azurerm_subnet.hp_subnet.id]
-#   }
+    network_rules {
+    default_action             = "Allow"
+    virtual_network_subnet_ids = [azurerm_subnet.hp_subnet.id]
+  }
 
-# }
-
-resource "azurerm_storage_container" "containerhp" {
-  name                  = "containerhp"
-  storage_account_name  = "himeshtest"
-  container_access_type = "blob"
 }
+
+# resource "azurerm_storage_container" "containerhp" {
+#   name                  = "containerhp"
+#   storage_account_name  = azurerm_storage_account.storageaccount.name
+#   container_access_type = "blob"
+# }
 
 # Had to use below config for container as above azurerm wasn't working 
 # resource "azapi_resource" "container" {
