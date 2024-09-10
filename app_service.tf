de1a8b932021db336aaf5568c4e309acb42fdefc
himesh99 <himesh.patel@hmcts.net>
2024-09-10 20:48:44 +0100

    Name change

--- app_service.tf
+++ app_service.tf
@@ -1,7 +1,7 @@
-resource "azurerm_app_service" "wordpresswebapp" {
-   name  = "wordpresswebapp"
+resource "azurerm_app_service" "webapp" {
+   name  = "webapp"
     resource_group_name = var.resource_group_hp
     location = var.location
-    app_service_plan_id = azurerm_app_service_plan.wordpress_plan.id
+    app_service_plan_id = azurerm_app_service_plan.webapp_plan.id
     tags = var.tags
 }
