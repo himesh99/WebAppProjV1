5f82bc272812d568b27b659e0a57680ca816aa2e
himesh99 <himesh.patel@hmcts.net>
2024-09-10 21:11:40 +0100

    changes v2

--- app_service.tf
+++ app_service.tf
@@ -4,4 +4,23 @@ resource "azurerm_app_service" "webapp" {
     location = var.location
     app_service_plan_id = azurerm_app_service_plan.webapp_plan.id
     tags = var.tags
+    site_config {
+     always_on = true
+   }
+   app_settings = {
+     "VaultName" = azurerm_key_vault.himeshkv.name
+   }
+   connection_string {
+     name = "SQLAZDBConnstr"
+     type = "SQLAzure"
+     value = azurerm_sql_server.hp_server.connection_strings[0].value
+   }
+}
+
+resource "azurerm_app_service_source_control" "sourcecontrol" {
+  app_id             = azurerm_linux_web_app.webapp.id
+  repo_url           = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
+  branch             = "master"
+  use_manual_integration = true
+  use_mercurial      = false
 }
