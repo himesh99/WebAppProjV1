1bb9bd85dd9fb40a037f64fb9bef507977e7a47f
himesh99 <himesh.patel@hmcts.net>
2024-09-12 13:34:02 +0100

    Comment out sitconfig, app seetings etc

--- app_service.tf
+++ app_service.tf
@@ -4,17 +4,17 @@ resource "azurerm_app_service" "webapp" {
     location = var.location
     app_service_plan_id = azurerm_app_service_plan.webapp_plan.id
     tags = var.tags
-    site_config {
-     always_on = true
-   }
-   app_settings = { 
-     "VaultName" = azurerm_key_vault.himeshkv.name
-   }
-   connection_string {
-     name = "SQLAZDBConnstr"
-     type = "SQLAzure"
-     value = "Server=tcp:${azurerm_sql_server.hp_server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_server.hp_server.name};Persist Security Info=False;User ID=${azurerm_sql_server.hp_server.administrator_login};Password=${azurerm_sql_server.hp_server.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
-   }
+  #   site_config {
+  #    always_on = true
+  #  }
+  #  app_settings = { 
+  #    "VaultName" = azurerm_key_vault.himeshkv.name
+  #  }
+  #  connection_string {
+  #    name = "SQLAZDBConnstr"
+  #    type = "SQLAzure"
+  #    value = "Server=tcp:${azurerm_sql_server.hp_server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_server.hp_server.name};Persist Security Info=False;User ID=${azurerm_sql_server.hp_server.administrator_login};Password=${azurerm_sql_server.hp_server.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  #  }
 }
 
 # resource "azurerm_app_service_source_control" "sourcecontrol" {
