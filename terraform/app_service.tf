5725e7e8483e1789d6af7266837043d6ac518459
himesh99 <himesh.patel@hmcts.net>
2024-09-16 20:25:52 +0100

    fix code errors

--- terraform/app_service.tf
+++ terraform/app_service.tf
@@ -2,7 +2,7 @@ resource "azurerm_app_service" "webapp" {
     name  = "webapp-deployment-test-001"
     resource_group_name = var.resource_group_hp
     location = var.location
-    app_service_plan_id = azurerm_app_service_plan.app-deployment-dev.id
+    app_service_plan_id = azurerm_app_service_plan.app-deployment.id
     tags = var.tags
 
 }
--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -1,5 +1,5 @@
 resource "azurerm_key_vault" "webappkv" {
-    name  = "webpp_kv"
+    name  = "webppkv"
     resource_group_name = var.resource_group_hp
     location = var.location
     sku_name = "standard"
