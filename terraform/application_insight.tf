9c7c6c9c6eea12f3c92cc13c5684ee32f76ab5e3
himesh99 <himesh.patel@hmcts.net>
2024-09-29 20:39:52 +0100

    Remove app insights

--- terraform/application_insight.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_application_insights" "app_insights" {
-  name                = "app-insights-001"
-  location            = var.location
-  resource_group_name = var.resource_group_hp
-  application_type    = "web"
-  tags                = var.tags
-}
