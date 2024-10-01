ff37fb6a7070dafe9d074aed0715ea81309baf7e
himesh99 <himesh.patel@hmcts.net>
2024-10-01 17:19:40 +0100

    disable rbac

--- terraform/key_vault.tf
+++ terraform/key_vault.tf
@@ -5,7 +5,6 @@ resource "azurerm_key_vault" "webappkv" {
     sku_name = "standard"
     tenant_id = var.tenant_id
     tags = var.tags
-    enable_rbac_authorization = true
 
 }
 
@@ -37,11 +36,39 @@ resource "azurerm_key_vault_access_policy" "webappkv_access_policy" {
       ]
     }
 
+resource "azurerm_key_vault_access_policy" "webappkv_access_policy_2" {
+      key_vault_id = azurerm_key_vault.webappkv.id
+      tenant_id = var.tenant_id
+      object_id = "0e4b967e-0513-4269-aa22-687166d31276"
+
+      key_permissions = [
+        "Get",
+        "List",
+        "Update",
+        "Create",
+        "Import",
+        "Delete",
+        "Recover",
+        "Backup",
+        "Restore",
+      ]
+
+      secret_permissions = [
+        "Get",
+        "List",
+        "Set",
+        "Delete",
+        "Recover",
+        "Backup",
+        "Restore",
+      ]
+    }
+
 resource "azurerm_key_vault_secret" "sql_server_admin_password" {
   name   = "sql-server-admin-password"
   value = azurerm_sql_server.sqlsvr.administrator_login_password
   key_vault_id = azurerm_key_vault.webappkv.id
   tags         = var.tags
   content_type = "password"
-  depends_on = [azurerm_key_vault_access_policy.webappkv_access_policy]
+  depends_on = [azurerm_key_vault_access_policy.webappkv_access_policy, azurerm_key_vault_access_policy.webappkv_access_policy_2]
 }
