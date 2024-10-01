27a87151730cf3b60d82248c2d6cba622d84c51a
Himesh <himesh.patel99@hotmail.com>
2024-10-01 15:50:25 +0100

    remove private endpoint

--- terraform/private_endpoint.tf
+++ terraform/private_endpoint.tf
@@ -12,19 +12,19 @@ resource "azurerm_private_endpoint" "storage_private_endpoint" {
   }
 }
 
-resource "azurerm_private_endpoint" "keyvault_private_endpoint" {
-  name                = "keyvault-private-endpoint-001"
-  location            = var.location
-  resource_group_name = var.resource_group_hp
-  subnet_id           = azurerm_subnet.webapp_snet.id
+# resource "azurerm_private_endpoint" "keyvault_private_endpoint" {
+#   name                = "keyvault-private-endpoint-001"
+#   location            = var.location
+#   resource_group_name = var.resource_group_hp
+#   subnet_id           = azurerm_subnet.webapp_snet.id
 
-  private_service_connection {
-    name                           = "keyvault-private-connection-001"
-    private_connection_resource_id = azurerm_key_vault.webappkv.id
-    is_manual_connection           = false
-    subresource_names              = ["vault"]
-  }
-}
+#   private_service_connection {
+#     name                           = "keyvault-private-connection-001"
+#     private_connection_resource_id = azurerm_key_vault.webappkv.id
+#     is_manual_connection           = false
+#     subresource_names              = ["vault"]
+#   }
+# }
 
 resource "azurerm_private_endpoint" "sql_private_endpoint" {
   name                = "sql-private-endpoint-001"
