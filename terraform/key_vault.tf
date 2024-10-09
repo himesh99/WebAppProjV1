44aeef0742e89ad6448090f012b21f199a8c87d6
himesh99 <himesh.patel@hmcts.net>
2024-10-09 17:44:09 +0100

    kv

--- terraform/key_vault.tf
+++ terraform/key_vault.tf
@@ -5,7 +5,7 @@ resource "azurerm_key_vault" "webappkv" {
   sku_name            = "standard"
   tenant_id           = var.tenant_id
   tags                = var.tags
-  public_network_access_enabled = false
+  public_network_access_enabled = true
 
   # Ensure to set the access policies here
   access_policy {
@@ -37,8 +37,8 @@ resource "azurerm_key_vault" "webappkv" {
   }
 
   network_acls {
-    bypass = "AzureServices"
-    default_action = "Allow"
+    bypass = "None"
+    default_action = "Deny"
     virtual_network_subnet_ids = [ azurerm_subnet.service.id]
   }
 
@@ -108,4 +108,3 @@ resource "azurerm_key_vault_secret" "sql_server_admin_password" {
   content_type = "password"
   depends_on   = [azurerm_key_vault_access_policy.access_policy_1]
 }
-
