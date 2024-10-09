cc0ef6a04e5033addf5914d304764018075fe86c
himesh99 <himesh.patel@hmcts.net>
2024-10-09 17:13:31 +0100

    naming change

--- terraform/key_vault.tf
+++ terraform/key_vault.tf
@@ -1,10 +1,11 @@
 resource "azurerm_key_vault" "webappkv" {
-  name                = "webapp-kv-001"
+  name                = "web-app-kv-001"
   resource_group_name = var.resource_group_hp
   location            = var.location
   sku_name            = "standard"
   tenant_id           = var.tenant_id
   tags                = var.tags
+  public_network_access_enabled = false
 
   # Ensure to set the access policies here
   access_policy {
