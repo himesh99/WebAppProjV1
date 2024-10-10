42cb3fd2850c731b9ccdb02ea863066660825b04
himesh99 <himesh.patel@hmcts.net>
2024-10-10 17:21:49 +0100

    Whitelist i[

--- terraform/key_vault.tf
+++ terraform/key_vault.tf
@@ -39,7 +39,7 @@ resource "azurerm_key_vault" "webappkv" {
     bypass = "AzureServices"
     default_action = "Deny"
     virtual_network_subnet_ids = [ azurerm_subnet.service.id]
-    ip_rules = ["84.69.79.240/32", "108.143.102.153/32"] 
+    ip_rules = ["84.69.79.240/32", "108.143.102.153/32", "108.142.210.216/32"] 
   }
 
 }
