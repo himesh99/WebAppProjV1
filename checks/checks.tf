5ee69b6cc7140368a41da5c0f17372f3f77fc47d
Himesh <himesh.patel99@hotmail.com>
2024-11-04 16:17:00 +0000

    remove quotes

--- checks/checks.tf
+++ checks/checks.tf
@@ -4,11 +4,11 @@ check "webapp" {
     error_message = "Name of web app is ${data.azurerm_app_service.webapp.name}, should be webapp-deployment-development-001"
   }
   assert {
-    condition     = data.azurerm_app_service.webapp.enabled == "true"
+    condition     = data.azurerm_app_service.webapp.enabled == true
     error_message = "Web app enabled is ${data.azurerm_app_service.webapp.enabled}, should be true"
   }
   assert {
-    condition     = data.azurerm_app_service.webapp.https_only == "true"
+    condition     = data.azurerm_app_service.webapp.https_only == true
     error_message = "Web app https only is ${data.azurerm_app_service.webapp.https_only}, should be true"
   }
 }
