b2727b3d31b2818716ceda41a4df650538c1e97e
Himesh <himesh.patel99@hotmail.com>
2024-11-04 16:14:14 +0000

    adding assert conditions

--- checks/checks.tf
+++ checks/checks.tf
@@ -1,6 +1,14 @@
 check "webapp" {
   assert {
-    condition     = data.azurerm_app_service.webapp == "webapp-deployment-development-001"
+    condition     = data.azurerm_app_service.webapp.name == "webapp-deployment-development-001"
     error_message = "Name of web app is ${data.azurerm_app_service.webapp.name}, should be webapp-deployment-development-001"
+  }
+  assert {
+    condition     = data.azurerm_app_service.webapp.enabled == "true"
+    error_message = "Web app enabled is ${data.azurerm_app_service.webapp.enabled}, should be true"
+  }
+  assert {
+    condition     = data.azurerm_app_service.webapp.https_only == "true"
+    error_message = "Web app https only is ${data.azurerm_app_service.webapp.https_only}, should be true"
   }
 }
