df1c7b43d40ee38605df3ba765bc446a6a6a880c
himesh99 <himesh.patel@hmcts.net>
2024-11-06 17:08:11 +0000

    tf fmt

--- terraform/app_service.tf
+++ terraform/app_service.tf
@@ -4,7 +4,7 @@ resource "azurerm_linux_web_app" "webapp" {
   location            = var.location
   tags                = var.tags
   site_config {
-     minimum_tls_version = "1.2"
+    minimum_tls_version = "1.2"
   }
   service_plan_id = azurerm_service_plan.webappservice.id
 
@@ -20,9 +20,9 @@ resource "azurerm_linux_web_app" "webapp" {
 
 #  Deploy code from a public GitHub repo
 resource "azurerm_app_service_source_control" "sourcecontrol" {
-  app_id             = azurerm_linux_web_app.webapp.id
-  repo_url           = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
-  branch             = "master"
+  app_id                 = azurerm_linux_web_app.webapp.id
+  repo_url               = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
+  branch                 = "master"
   use_manual_integration = true
-  use_mercurial      = false
+  use_mercurial          = false
 }
