c2c4b5c0d45ca55bb92ab34928f62c78ea8479a0
himesh99 <himesh.patel@hmcts.net>
2024-11-14 14:50:05 +0000

    fix outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,5 @@
-output "web_app_url" {
-  value       = azurerm_linux_web_app.webapp.default_site_hostname
+output "web_app" {
+  value       = azurerm_linux_web_app.webapp.name
   description = "The URL of the deployed web application."
 }
 
