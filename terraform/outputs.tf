06835f36dc751345122ee38500b2c7936d4b4493
himesh99 <himesh.patel@hmcts.net>
2024-11-14 14:34:20 +0000

    outputs

---
+++ terraform/outputs.tf
@@ -0,0 +1,29 @@
+output "web_app_url" {
+  value = azurerm_linux_web_app.webapp.default_site_hostname
+  description = "The URL of the deployed web application."
+}
+
+output "sql_server_name" {
+  value = azurerm_sql_server.sqlsvr.name
+  description = "The name of the SQL Server."
+}
+
+output "sql_database_name" {
+  value = azurerm_sql_database.sqldb.name
+  description = "The name of the SQL Database."
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.storageaccount.name
+  description = "The name of the Storage Account."
+}
+
+output "key_vault_name" {
+  value = azurerm_key_vault.webappkv.name
+  description = "The name of the Key Vault."
+}
+
+output "service_plan_id" {
+  value = azurerm_service_plan.webappservice.id
+  description = "The ID of the App Service Plan."
+}
