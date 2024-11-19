b7deff56f7e74e30bd375f1d7291c14d1bc856c8
himesh99 <himesh.patel@hmcts.net>
2024-11-19 19:43:55 +0000

    change to main

--- terraform/app_service.tf
+++ terraform/app_service.tf
@@ -19,11 +19,11 @@ resource "azurerm_linux_web_app" "webapp" {
 
 
 
-# #  Deploy code from a public GitHub repo
-# resource "azurerm_app_service_source_control" "sourcecontrol" {
-#   app_id                 = azurerm_linux_web_app.webapp.id
-#   repo_url               = "https://github.com/himesh99/himesh-hello-world"
-#   branch                 = "master"
-#   use_manual_integration = true
-#   use_mercurial          = false
-# }
+#  Deploy code from a public GitHub repo
+resource "azurerm_app_service_source_control" "sourcecontrol" {
+  app_id                 = azurerm_linux_web_app.webapp.id
+  repo_url               = "https://github.com/himesh99/himesh-hello-world"
+  branch                 = "main"
+  use_manual_integration = true
+  use_mercurial          = false
+}
