f8ff5e0329fe59b88a690e6f5ac6fab90699c7c7
himesh99 <himesh.patel@hmcts.net>
2024-11-25 15:14:52 +0000

    test

--- checks/data_sources.tf
+++ checks/data_sources.tf
@@ -3,12 +3,12 @@ data "azurerm_app_service" "webapp" {
   resource_group_name = var.resource_group_hp
 }
 
-data "azurerm_virtual_network" "webapp" {
+data "azurerm_virtual_network" "vnet" {
   name                = "vnet-shared-uksouth-${var.environment}-001"
   resource_group_name = var.resource_group_hp
 }
 
-data "azurerm_sql_database" "webapp" {
+data "azurerm_sql_database" "db" {
   name                = "sqldb-webapp-${var.environment}"
   resource_group_name = var.resource_group_hp
   server_name         = "sqlsvr-webapp-${var.environment}"
