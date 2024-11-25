2e656c39faf32d66125d16bdeaf586fad6ea2b61
himesh99 <himesh.patel@hmcts.net>
2024-11-25 15:48:37 +0000

    name change

--- checks/data_sources.tf
+++ checks/data_sources.tf
@@ -1,5 +1,5 @@
 data "azurerm_app_service" "webapp" {
-  name                = "webapp-deployment-${var.environment}-001"
+  name                = "app-web-${var.environment}-001"
   resource_group_name = var.resource_group_hp
 }
 
