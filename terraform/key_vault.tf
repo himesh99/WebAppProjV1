d4fd0c27fc4ed5fe46d54883cf677c8af4065e63
Himesh <himesh.patel99@hotmail.com>
2024-11-26 15:01:11 +0000

    undo policy comment

--- terraform/key_vault.tf
+++ terraform/key_vault.tf
@@ -23,67 +23,67 @@ resource "azurerm_key_vault" "webappkv" {
 
 }
 
-# resource "azurerm_key_vault_access_policy" "access_policy_1" {
-#   key_vault_id = azurerm_key_vault.webappkv.id
-#   tenant_id    = var.tenant_id
-#   object_id    = var.object_id
+resource "azurerm_key_vault_access_policy" "access_policy_1" {
+  key_vault_id = azurerm_key_vault.webappkv.id
+  tenant_id    = var.tenant_id
+  object_id    = var.object_id
 
-#   key_permissions = [
-#     "Get",
-#     "List",
-#     "Update",
-#     "Create",
-#     "Import",
-#     "Delete",
-#     "Recover",
-#     "Backup",
-#     "Restore",
-#   ]
+  key_permissions = [
+    "Get",
+    "List",
+    "Update",
+    "Create",
+    "Import",
+    "Delete",
+    "Recover",
+    "Backup",
+    "Restore",
+  ]
 
-#   secret_permissions = [
-#     "Get",
-#     "List",
-#     "Set",
-#     "Delete",
-#     "Recover",
-#     "Backup",
-#     "Restore",
-#   ]
-# }
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set",
+    "Delete",
+    "Recover",
+    "Backup",
+    "Restore",
+  ]
+}
 
-# resource "azurerm_key_vault_access_policy" "webappkv_access_policy_2" {
-#   key_vault_id = azurerm_key_vault.webappkv.id
-#   tenant_id    = var.tenant_id
-#   object_id    = "0e4b967e-0513-4269-aa22-687166d31276"
+resource "azurerm_key_vault_access_policy" "webappkv_access_policy_2" {
+  key_vault_id = azurerm_key_vault.webappkv.id
+  tenant_id    = var.tenant_id
+  object_id    = "0e4b967e-0513-4269-aa22-687166d31276"
 
-#   key_permissions = [
-#     "Get",
-#     "List",
-#     "Update",
-#     "Create",
-#     "Import",
-#     "Delete",
-#     "Recover",
-#     "Backup",
-#     "Restore",
-#   ]
+  key_permissions = [
+    "Get",
+    "List",
+    "Update",
+    "Create",
+    "Import",
+    "Delete",
+    "Recover",
+    "Backup",
+    "Restore",
+  ]
 
-#   secret_permissions = [
-#     "Get",
-#     "List",
-#     "Set",
-#     "Delete",
-#     "Recover",
-#     "Backup",
-#     "Restore",
-#   ]
-# }
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set",
+    "Delete",
+    "Recover",
+    "Backup",
+    "Restore",
+  ]
+}
 
-# resource "azurerm_key_vault_secret" "sql_server_admin_password" {
-#   name         = "sql-server-${var.environment}-password"
-#   value        = azurerm_sql_server.sqlsvr.administrator_login_password
-#   key_vault_id = azurerm_key_vault.webappkv.id
-#   tags         = var.tags
-#   content_type = "password"
-#   depends_on   = [azurerm_key_vault_access_policy.webappkv_access_policy_2]
-# }
+resource "azurerm_key_vault_secret" "sql_server_admin_password" {
+  name         = "sql-server-${var.environment}-password"
+  value        = azurerm_sql_server.sqlsvr.administrator_login_password
+  key_vault_id = azurerm_key_vault.webappkv.id
+  tags         = var.tags
+  content_type = "password"
+  depends_on   = [azurerm_key_vault_access_policy.webappkv_access_policy_2]
+}
