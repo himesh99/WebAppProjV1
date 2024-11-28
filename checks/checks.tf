2a42078f524539a857f7d0c8caeabdbc7e57596d
himesh99 <himesh.patel@hmcts.net>
2024-11-28 11:31:08 +0000

    add fail condition

--- checks/checks.tf
+++ checks/checks.tf
@@ -10,6 +10,7 @@ check "webapp" {
   assert {
     condition     = data.azurerm_app_service.webapp.https_only == true
     error_message = "Web app https only is ${data.azurerm_app_service.webapp.https_only}, should be true"
+    fail          = true
   }
 }
 
