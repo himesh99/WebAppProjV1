7d1cb2511378839dbd9ccbebfdd72262c9bcca74
himesh99 <himesh.patel@hmcts.net>
2024-12-02 10:46:33 +0000

    fix checks

--- checks/checks.tf
+++ checks/checks.tf
@@ -18,7 +18,7 @@ check "vnet" {
 
 check "db" {
   assert {
-    condition     = data.azurerm_sql_database.edition == "Standard"
+    condition     = data.azurerm_sql_database.sqldb.edition == "Standard"
     error_message = "All SQL databases should have an Standard edition, please change edition if not stnadard"
   }
 }
