74105444bf60c20ed237ae0514f73319cbd932d3
himesh99 <himesh.patel@hmcts.net>
2024-12-06 12:29:46 +0000

    tidy code

--- terraform/subnet.tf
+++ terraform/subnet.tf
@@ -12,7 +12,7 @@ resource "azurerm_subnet" "service" {
   name                 = "snet-shared-uksouth-${var.environment}-002"
   resource_group_name  = var.resource_group_hp
   address_prefixes     = ["10.0.2.0/24"]
-  service_endpoints    = ["Microsoft.KeyVault", "Microsoft.Storage", "Microsoft.Web"]
+  service_endpoints    = ["Microsoft.KeyVault", "Microsoft.Storage"]
   virtual_network_name = azurerm_virtual_network.webapp_vnet.name
 
 
