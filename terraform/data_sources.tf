fc1c70aaff7bae503a36915abeb9c088020fc8eb
himesh99 <himesh.patel@hmcts.net>
2025-01-06 16:49:36 +0000

    comment data sources

--- terraform/data_sources.tf
+++ terraform/data_sources.tf
@@ -1,25 +1,25 @@
-data "azurerm_key_vault" "config_kv" {
-  name                = "kv-backend"
-  resource_group_name = var.resource_group_hp
-}
+# data "azurerm_key_vault" "config_kv" {
+#   name                = "kv-backend"
+#   resource_group_name = var.resource_group_hp
+# }
 
-data "azurerm_key_vault_secret" "tenant_id" {
-  name         = "tenantid"
-  key_vault_id = data.azurerm_key_vault.config_kv.id
-}
+# data "azurerm_key_vault_secret" "tenant_id" {
+#   name         = "tenantid"
+#   key_vault_id = data.azurerm_key_vault.config_kv.id
+# }
 
-data "azurerm_key_vault_secret" "sa_access_key" {
-  name         = "saaccesskey"
-  key_vault_id = data.azurerm_key_vault.config_kv.id
-}
+# data "azurerm_key_vault_secret" "sa_access_key" {
+#   name         = "saaccesskey"
+#   key_vault_id = data.azurerm_key_vault.config_kv.id
+# }
 
-data "azurerm_key_vault_secret" "object_id" {
-  name         = "objectid"
-  key_vault_id = data.azurerm_key_vault.config_kv.id
-}
+# data "azurerm_key_vault_secret" "object_id" {
+#   name         = "objectid"
+#   key_vault_id = data.azurerm_key_vault.config_kv.id
+# }
 
-data "azurerm_key_vault_secret" "v1_object_id" {
-  name         = "v1objectid"
-  key_vault_id = data.azurerm_key_vault.config_kv.id
-}
+# data "azurerm_key_vault_secret" "v1_object_id" {
+#   name         = "v1objectid"
+#   key_vault_id = data.azurerm_key_vault.config_kv.id
+# }
 
