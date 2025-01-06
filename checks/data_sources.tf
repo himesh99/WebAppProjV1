6d011ac61ea42fc2bfbce4532aea024e30bced6c
himesh99 <himesh.patel@hmcts.net>
2025-01-06 16:42:40 +0000

    comment out data sources

--- checks/data_sources.tf
+++ checks/data_sources.tf
@@ -1,14 +1,14 @@
-data "azurerm_app_service" "webapp" {
-  name                = "app-web-${var.environment}-001"
-  resource_group_name = var.resource_group_hp
-}
+# data "azurerm_app_service" "webapp" {
+#   name                = "app-web-${var.environment}-001"
+#   resource_group_name = var.resource_group_hp
+# }
 
-data "azurerm_virtual_network" "webapp" {
-  name                = "vnet-shared-uksouth-${var.environment}-001"
-  resource_group_name = var.resource_group_hp
-}
+# data "azurerm_virtual_network" "webapp" {
+#   name                = "vnet-shared-uksouth-${var.environment}-001"
+#   resource_group_name = var.resource_group_hp
+# }
 
-data "azurerm_key_vault" "webapp" {
-  name                = "kv-${var.environment}-uks-001"
-  resource_group_name = var.resource_group_hp
-}
+# data "azurerm_key_vault" "webapp" {
+#   name                = "kv-${var.environment}-uks-001"
+#   resource_group_name = var.resource_group_hp
+# }
