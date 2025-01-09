cfbf63849c7d39201b5cb69eb42e72921cbc0852
himesh99 <himesh.patel@hmcts.net>
2025-01-09 16:12:42 +0000

    fix

--- terraform/sql_database.tf
+++ terraform/sql_database.tf
@@ -1,9 +1,7 @@
 resource "azurerm_mysql_database" "sqldb" {
-  name                             = "sqldb-webapp-${var.environment}"
-  resource_group_name              = var.resource_group_hp
-  location                         = var.location
-  server_name                      = azurerm_mysql_server.sqlsvr.name
-  edition                          = "Standard"
-  requested_service_objective_name = "S1"
-  tags                             = var.tags
+  name                = "sqldb-webapp-${var.environment}"
+  resource_group_name = var.resource_group_hp
+  server_name         = azurerm_mysql_server.sqlsvr.name
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
 }
