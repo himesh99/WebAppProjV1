e017fc940ca57f3fe36112ecaa356167a2024c01
himesh99 <himesh.patel@hmcts.net>
2025-01-09 12:53:13 +0000

    fmt

--- terraform/app_service.tf
+++ terraform/app_service.tf
@@ -1,15 +1,14 @@
-resource "azurerm_linux_web_app" "webapp" {
+resource "azurerm_web_app" "webapp" {
   name                = "app-web-${var.environment}"
   resource_group_name = var.resource_group_hp
   location            = var.location
   tags                = var.tags
   site_config {
     minimum_tls_version = "1.2"
-    application_stack {
-      docker_image_name = "DOCKER/wordpress:php8.3-fpm-alpine"
-    }
+    linux_fx_version    = "DOCKER/wordpress:php8.3-fpm-alpine"
+
   }
-  service_plan_id = azurerm_service_plan.webappservice.id
+  app_service_plan_id = azurerm_service_plan.webappservice.id
 
 
 
