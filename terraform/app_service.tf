09afe5dcb4f15e730c65cc079b847d6753b56014
himesh99 <himesh.patel@hmcts.net>
2025-01-10 15:59:37 +0000

    linux

--- terraform/app_service.tf
+++ terraform/app_service.tf
@@ -1,13 +1,12 @@
-resource "azurerm_app_service" "webapp" {
+resource "azurerm_linux_web_app" "webapp" {
   name                = "app-web-${var.environment}"
   resource_group_name = var.resource_group_hp
   location            = var.location
   tags                = var.tags
-  app_service_plan_id = azurerm_service_plan.webappservice.id
+  service_plan_id = azurerm_service_plan.webappservice.id
 
   site_config {
     always_on        = true
-    min_tls_version  = 1.2
     linux_fx_version = "DOCKER|mcr.microsoft.com/appsvc/wordpress-debian-php:8.3"
   }
 
