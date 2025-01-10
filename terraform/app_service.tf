91c221904c3e5b1c5d064388902a4ed34a19633c
himesh99 <himesh.patel@hmcts.net>
2025-01-10 16:10:22 +0000

    fmt

--- terraform/app_service.tf
+++ terraform/app_service.tf
@@ -3,7 +3,7 @@ resource "azurerm_linux_web_app" "webapp" {
   resource_group_name = var.resource_group_hp
   location            = var.location
   tags                = var.tags
-  service_plan_id = azurerm_service_plan.webappservice.id
+  service_plan_id     = azurerm_service_plan.webappservice.id
 
   site_config {
     always_on        = true
