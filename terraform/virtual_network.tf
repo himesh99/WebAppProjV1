dd6be0a90341f95ee9854a77eee3e91589459884
himesh99 <himesh.patel@hmcts.net>
2025-03-17 12:03:50 +0000

    fix

--- terraform/virtual_network.tf
+++ terraform/virtual_network.tf
@@ -2,6 +2,6 @@ resource "azurerm_virtual_network" "webapp_vnet" {
   resource_group_name = var.resource_group_hp
   name                = "vnet-shared-uksouth-${var.environment}"
   location            = var.location
-  address_space       = var.address_space
+  address_space       = [var.address_space]
   tags                = var.tags
 }
