859c8c758553ffd3b484d20c7a740bfecafdd2be
himesh99 <himesh.patel@hmcts.net>
2025-05-14 15:19:45 +0100

    codefix

--- terraform/storage_account.tf
+++ terraform/storage_account.tf
@@ -9,7 +9,7 @@ resource "azurerm_storage_account" "storageaccount" {
   tags                            = var.tags
   network_rules {
     default_action             = "Deny"
-    ip_rules                   = ["84.69.91.47", "20.61.71.107", "52.236.93.30", "13.79.42.117"]
+    ip_rules                   = ["84.69.91.47", "20.61.71.107", "52.236.93.30", "13.79.42.117", "13.79.43.156"]
     virtual_network_subnet_ids = [azurerm_subnet.service.id]
   }
 }
