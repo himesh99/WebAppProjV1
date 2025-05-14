da94624a1b0e64105a95c9d36aad08df2dc22e33
himesh99 <himesh.patel@hmcts.net>
2025-05-14 10:48:02 +0100

    codefix

--- terraform/sql_server.tf
+++ terraform/sql_server.tf
@@ -24,55 +24,18 @@ resource "azurerm_mysql_flexible_server_configuration" "require_secure_transport
   name                = "require_secure_transport"
   resource_group_name = var.resource_group_hp
   server_name         = azurerm_mysql_flexible_server.sqlsvr.name
-  value               = "off"
+  value               = "on"
 
 }
 
 resource "azurerm_mysql_flexible_server_firewall_rule" "allow_all_ips" {
-  name                = "all_ip"
+  name                = "my_ip"
   resource_group_name = var.resource_group_hp
   server_name         = azurerm_mysql_flexible_server.sqlsvr.name
   start_ip_address    = "84.69.91.47"
   end_ip_address      = "84.69.91.47"
 }
 
-resource "azurerm_private_dns_zone" "sqlsvr" {
-  name                = "privatelink.mysqlsvr.database.azure.com"
-  resource_group_name = var.resource_group_hp
-
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "sqlsvrLink" {
-  name                  = "sqlsvr-vnet-link"
-  resource_group_name   = var.resource_group_hp
-  private_dns_zone_name = azurerm_private_dns_zone.sqlsvr.name
-  virtual_network_id    = azurerm_virtual_network.webapp_vnet.id
-}
-
-resource "azurerm_private_dns_a_record" "sqlsvrArecord" {
-  name                = "sqlsvr-webapp-dev"
-  zone_name           = azurerm_private_dns_zone.sqlsvr.name
-  resource_group_name = var.resource_group_hp
-  ttl                 = 10
-  records             = ["10.0.1.5"]
-}
 
 
-resource "azurerm_private_endpoint" "database_private_endpoint" {
-  name                = "pe-svr-${var.environment}-uks"
-  location            = var.location
-  resource_group_name = var.resource_group_hp
-  subnet_id           = azurerm_subnet.database.id
-
-  private_service_connection {
-    name                           = "database-private-connection-${var.environment}"
-    private_connection_resource_id = azurerm_mysql_flexible_server.sqlsvr.id
-    is_manual_connection           = false
-    subresource_names              = ["mysqlServer"]
-  }
-  private_dns_zone_group {
-    name                 = "sqlsvr-dns-zone-group"
-    private_dns_zone_ids = [azurerm_private_dns_zone.sqlsvr.id]
-  }
-}
 
