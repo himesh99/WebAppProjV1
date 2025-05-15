a7fbbe3490e2e8275daa525576fa172d543b286d
himesh99 <himesh.patel@hmcts.net>
2025-05-15 09:43:24 +0100

    codefix

--- terraform/key_vault.tf
+++ terraform/key_vault.tf
@@ -9,7 +9,7 @@ resource "azurerm_key_vault" "webappkv" {
   network_acls {
     default_action             = "Deny"
     bypass                     = "AzureServices"
-    ip_rules                   = ["84.69.91.47", "20.61.71.107", "52.236.93.30", "13.79.42.117", "13.79.43.156", "52.236.93.29", "4.211.179.6"]
+    ip_rules                   = ["84.69.91.47", "20.61.71.107", "52.236.93.30", "13.79.42.117", "13.79.43.156", "52.236.93.29", "4.211.179.6", " 137.135.191.38"]
     virtual_network_subnet_ids = [azurerm_subnet.service.id]
   }
 }
