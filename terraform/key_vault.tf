86507073c4c8722f81bb7f22d642870273ac5bc6
himesh99 <himesh.patel@hmcts.net>
2025-05-16 21:24:03 +0100

    codefix

--- terraform/key_vault.tf
+++ terraform/key_vault.tf
@@ -8,7 +8,7 @@ resource "azurerm_key_vault" "webappkv" {
   public_network_access_enabled = true
 
   network_acls {
-    default_action = "Allow"
+    default_action = "Deny"
     bypass         = "AzureServices"
     ip_rules = [
       "84.69.91.47"
