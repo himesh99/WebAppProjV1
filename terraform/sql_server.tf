0794ca1c1289fd6f5cdac067eb1493ed5f37f9ee
himesh99 <himesh.patel@hmcts.net>
2025-05-21 20:25:28 +0100

    codefix

--- terraform/sql_server.tf
+++ terraform/sql_server.tf
@@ -4,15 +4,15 @@ locals {
 
 #tst
 resource "azurerm_mysql_flexible_server" "sqlsvr" {
-  name                   = "sqlsvr-wp-${var.environment}-uks"
-  resource_group_name    = var.resource_group_hp
-  location               = var.location
-  administrator_login    = local.sql_server_username
-  administrator_password = azurerm_key_vault_secret.sql_server_admin_password.value
-  sku_name               = "GP_Standard_D2ds_v4"
-  zone                   = "1"
-  private_dns_zone_id    = azurerm_private_dns_zone.sqlsvr.id
-  depends_on             = [azurerm_private_dns_zone_virtual_network_link.sqlsvrLink]
+  name                          = "sqlsvr-wp-${var.environment}-uks"
+  resource_group_name           = var.resource_group_hp
+  location                      = var.location
+  administrator_login           = local.sql_server_username
+  administrator_password        = azurerm_key_vault_secret.sql_server_admin_password.value
+  sku_name                      = "GP_Standard_D2ds_v4"
+  zone                          = "1"
+  private_dns_zone_id           = azurerm_private_dns_zone.sqlsvr.id
+  depends_on                    = [azurerm_private_dns_zone_virtual_network_link.sqlsvrLink]
   public_network_access_enabled = true
 
 }
