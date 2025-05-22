a2bc2936f5ce378eb5c512358f3c86b944888c31
himesh99 <himesh.patel@hmcts.net>
2025-05-23 00:18:18 +0100

    codefix

--- terraform/sql_server.tf
+++ terraform/sql_server.tf
@@ -4,18 +4,24 @@ locals {
 
 #tst
 resource "azurerm_mysql_flexible_server" "sqlsvr" {
-  name                          = "sqlsvr-wp-${var.environment}-uks"
-  resource_group_name           = var.resource_group_hp
-  location                      = var.location
-  administrator_login           = local.sql_server_username
-  administrator_password        = azurerm_key_vault_secret.sql_server_admin_password.value
-  sku_name                      = "GP_Standard_D2ds_v4"
-  zone                          = "1"
-  public_network_access_enabled = true
-  private_dns_zone_id           = azurerm_private_dns_zone.sqlsvr.id
+  name                   = "sqlsvr-wp-${var.environment}-uks"
+  resource_group_name    = var.resource_group_hp
+  location               = var.location
+  administrator_login    = local.sql_server_username
+  administrator_password = azurerm_key_vault_secret.sql_server_admin_password.value
+  sku_name               = "GP_Standard_D2ds_v4"
+  zone                   = "1"
+  private_dns_zone_id    = azurerm_private_dns_zone.sqlsvr.id
 
 }
 
+resource "azurerm_mysql_flexible_server_configuration" "require_secure_transport" {
+  name                = "require_secure_transport"
+  resource_group_name = var.resource_group_hp
+  server_name         = azurerm_mysql_flexible_server.sqlsvr.name
+  value               = "on"
+
+}
 
 resource "azurerm_mysql_flexible_server_firewall_rule" "allow_my_ip" {
   name                = "my_ip"
