62686773aa312ed62a814c619673d28f6034ac55
himesh99 <himesh.patel@hmcts.net>
2025-05-22 09:00:53 +0100

    codefix

--- terraform/app_service.tf
+++ terraform/app_service.tf
@@ -26,9 +26,7 @@ resource "azurerm_app_service" "webapp" {
     "WORDPRESS_LOCALE_CODE"                 = "en_US"
     "DOCKER_REGISTRY_SERVER_URL"            = "https://mcr.microsoft.com"
     "AZURE_STORAGE_CONNECTION_STRING"       = azurerm_storage_account.storageaccount.primary_connection_string
-    "WORDPRESS_USER"                        = "himesh.patel"
     "WORDPRESS_PASSWORD"                    = azurerm_key_vault_secret.wp_password.value
-    "WORDPRESS TITLE"                       = "Himesh Web App Demo"
   }
 
   connection_string {
